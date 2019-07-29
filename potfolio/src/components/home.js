8df12a448a68c513505dfbe02df3a2f53c43c69c
claire muwanguzi <kclaire56@yahoo.com>
2019-07-28 19:28:01 -0700

    initial commit

---
+++ potfolio/src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import portifolioImg from '../images/picDress.jpg';
+import ZyzyImg from '../images/Zz.gif';
+import shelter from '../images/Shelter.png';
+import techImg from '../images/techBot.gif';
+import google from '../images/googleNest.png';
+import jh from '../images/jh.jpg';
+import python from '../images/skills/python.png';
+import js from '../images/skills/js.png';
+import sql from '../images/skills/sql.png';
+import json from '../images/skills/json.png';
+
+class Home extends Component{
+
+}
+
+export default Home;
