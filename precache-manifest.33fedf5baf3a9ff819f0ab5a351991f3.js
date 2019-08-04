cdd33685afe2a274f229f71e42eba3dbdb6a2d64
claire muwanguzi <kclaire56@yahoo.com>
2019-08-03 21:14:50 -0700

    Updates

---
+++ precache-manifest.33fedf5baf3a9ff819f0ab5a351991f3.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1994db8ddde4492ae5daa209c6ed37f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cf5f56a57bab6274eb3",
+    "url": "/static/css/main.d4b6cb5e.chunk.css"
+  },
+  {
+    "revision": "c60aec886d167b019d98",
+    "url": "/static/js/2.abbe9f27.chunk.js"
+  },
+  {
+    "revision": "7cf5f56a57bab6274eb3",
+    "url": "/static/js/main.904883c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1f26ea5ae2f0458bfa69227efd6268c",
+    "url": "/static/media/HTML5logo.d1f26ea5.png"
+  },
+  {
+    "revision": "f58bf2428580f7fb0f60c7208bfec8b2",
+    "url": "/static/media/Java.f58bf242.jpg"
+  },
+  {
+    "revision": "07065f545d7fa66ae18e3b436db2461c",
+    "url": "/static/media/JavaScript.07065f54.png"
+  },
+  {
+    "revision": "9eeff617f03a031dc1f6e0d8327a7085",
+    "url": "/static/media/Numpy.9eeff617.png"
+  },
+  {
+    "revision": "9423dbce6b471cbeb702b52934235bd7",
+    "url": "/static/media/Shelter.9423dbce.png"
+  },
+  {
+    "revision": "6840084d226b5efbcf2e2289ec709b26",
+    "url": "/static/media/Zz.6840084d.gif"
+  },
+  {
+    "revision": "df488825ca0dee9e5e427f600fdd0e39",
+    "url": "/static/media/ajax.df488825.jpg"
+  },
+  {
+    "revision": "8dfd88da6a60c6cbb684d12e53aaa612",
+    "url": "/static/media/anaconda.8dfd88da.png"
+  },
+  {
+    "revision": "fb8e560e768c286cc367a060302609ed",
+    "url": "/static/media/aws.fb8e560e.jpg"
+  },
+  {
+    "revision": "64754f56b5d8363754b21db531a5e95e",
+    "url": "/static/media/claire.64754f56.jpg"
+  },
+  {
+    "revision": "04cf122be0afe7542083d1a3967390ae",
+    "url": "/static/media/css.04cf122b.png"
+  },
+  {
+    "revision": "1ab2f2b46abfad8ea8a41c7b60e5c589",
+    "url": "/static/media/django.1ab2f2b4.png"
+  },
+  {
+    "revision": "fe0e0a386f771ff0380e00c0eecf95b9",
+    "url": "/static/media/gcp.fe0e0a38.jpg"
+  },
+  {
+    "revision": "67b4ea91c0e2e436765b2bc3e70e2f65",
+    "url": "/static/media/googleNest.67b4ea91.png"
+  },
+  {
+    "revision": "8ae561ca6328eae05472c046b580d336",
+    "url": "/static/media/jh.8ae561ca.jpg"
+  },
+  {
+    "revision": "450f2b96f7e9a4dd6bd4203e9ae7d6d1",
+    "url": "/static/media/jquery.450f2b96.png"
+  },
+  {
+    "revision": "7f130501681cb3d0129f266820c78b24",
+    "url": "/static/media/js.7f130501.png"
+  },
+  {
+    "revision": "e91ce9398fe8b1aca84aa46eaac5b72d",
+    "url": "/static/media/mysql.e91ce939.png"
+  },
+  {
+    "revision": "f8b2bf7eb9f8a1ee124902bdc5da87ac",
+    "url": "/static/media/oracle.f8b2bf7e.png"
+  },
+  {
+    "revision": "d7f6dc80acc36e09ae5ff22fba5259d4",
+    "url": "/static/media/pandas.d7f6dc80.jpg"
+  },
+  {
+    "revision": "086f2a4de4eea1e8bbf7a2dd49c4f66f",
+    "url": "/static/media/postgresql.086f2a4d.png"
+  },
+  {
+    "revision": "6b002b1574dcd9395432f974e48cc53d",
+    "url": "/static/media/sK-Learn.6b002b15.jpg"
+  },
+  {
+    "revision": "c7fcfe510af62f29808eca5e2671c45c",
+    "url": "/static/media/sas.c7fcfe51.jpg"
+  },
+  {
+    "revision": "97dfe0eb2e5248b501065040234ca5f4",
+    "url": "/static/media/site.97dfe0eb.gif"
+  },
+  {
+    "revision": "233670d150cc021f531eb1c8bcbf5390",
+    "url": "/static/media/sql.233670d1.jpg"
+  },
+  {
+    "revision": "f81d68a326fd7d49a498bb14d4e033f0",
+    "url": "/static/media/techBot.f81d68a3.gif"
+  }
+]);
